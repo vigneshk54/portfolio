477f4cd3340a40904b177e6ebff582f4a1e85c22
Vignesh Kannan <apple@Vigneshs-MacBook-Pro.local>
2018-12-07 16:59:03 +0530

    few spellchecks

---
+++ precache-manifest.45988419e14f798261c104786c99552d.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "853102c3a00d184e1b8fe0f6b87bb605",
+    "url": "/static/media/IMG_0550.853102c3.JPG"
+  },
+  {
+    "revision": "b11c847698d100cc4df0",
+    "url": "/static/css/main.7b18ca09.chunk.css"
+  },
+  {
+    "revision": "a7b85b3845323bd8de6e",
+    "url": "/static/js/1.a7b85b38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3556eb5d1a0eca411d6a0e2bd6d08956",
+    "url": "/static/media/Edureka.3556eb5d.pdf"
+  },
+  {
+    "revision": "24fc90718e06ff2045b05acc2db924e3",
+    "url": "/static/media/bw-logo.24fc9071.png"
+  },
+  {
+    "revision": "343d5f9996705ce7ad92510f04e81e57",
+    "url": "/static/media/Vignesh_Kannan-Resume.343d5f99.pdf"
+  },
+  {
+    "revision": "f74e3363ea10d553e808cfb3c490e272",
+    "url": "/static/media/HPImage.f74e3363.png"
+  },
+  {
+    "revision": "0ec32239a17ca98493326df3d4a99546",
+    "url": "/static/media/Bargav.0ec32239.jpg"
+  },
+  {
+    "revision": "ed4593ef84a8d4e71fa6e955d5079761",
+    "url": "/static/media/Varun.ed4593ef.jpg"
+  },
+  {
+    "revision": "b62f2b200242d69707e1f0e9693a95da",
+    "url": "/static/media/Tejashree.b62f2b20.jpeg"
+  },
+  {
+    "revision": "2a6eefc081f3f8e75f86bae0047f2b18",
+    "url": "/static/media/practo-mobile.2a6eefc0.png"
+  },
+  {
+    "revision": "b6b30b472e0fee42fb0607bfab8731c7",
+    "url": "/static/media/user-personas.b6b30b47.png"
+  },
+  {
+    "revision": "d61f21652b5c0ffcb85b84635066020e",
+    "url": "/static/media/practo-arch.d61f2165.png"
+  },
+  {
+    "revision": "284791c367390d94035cbe0d24f91e7c",
+    "url": "/static/media/card-design-process.284791c3.png"
+  },
+  {
+    "revision": "833d8d647a3cbe56d12875e23f5c5c60",
+    "url": "/static/media/final-product.833d8d64.png"
+  },
+  {
+    "revision": "408a23f16cf91004c898707b21061dbc",
+    "url": "/static/media/practo_op.408a23f1.png"
+  },
+  {
+    "revision": "ba4c3f674eedb99e57458da59c3141c9",
+    "url": "/static/media/1.ba4c3f67.jpg"
+  },
+  {
+    "revision": "f1184d7af074fd07a93427e846f0badb",
+    "url": "/static/media/brwash3fiii.f1184d7a.jpg"
+  },
+  {
+    "revision": "c04d26464373a61b74644660032d599c",
+    "url": "/static/media/wild.c04d2646.png"
+  },
+  {
+    "revision": "0738fdef7f0f2960168684d438a76dbe",
+    "url": "/static/media/IMG_0497.0738fdef.JPG"
+  },
+  {
+    "revision": "b11c847698d100cc4df0",
+    "url": "/static/js/main.b11c8476.chunk.js"
+  },
+  {
+    "revision": "17fcba7a6cea2e63a5dcf12e3f5543b3",
+    "url": "/static/media/IMG_0579.17fcba7a.JPG"
+  },
+  {
+    "revision": "b7fa98249e74bd99ecf44fa110222908",
+    "url": "/static/media/tyrionf3.b7fa9824.jpg"
+  },
+  {
+    "revision": "2fb968d611fd5b9967660a44c1795969",
+    "url": "/static/media/bagfi.2fb968d6.jpg"
+  },
+  {
+    "revision": "4832c4a385779f62b9ccec320fabf589",
+    "url": "/static/media/Workstation.4832c4a3.jpg"
+  },
+  {
+    "revision": "b17679d54443904760a7ec56241567bb",
+    "url": "/static/media/CopyofFest_teaser.b17679d5.mp4"
+  },
+  {
+    "revision": "0d6d7a48abaf3ac53bbd6291008d8327",
+    "url": "/static/media/CopyofMarathon.0d6d7a48.mp4"
+  },
+  {
+    "revision": "9cbbeb55ad549362f356ca1cfcf39c6c",
+    "url": "/static/media/Focus.9cbbeb55.mp4"
+  },
+  {
+    "revision": "afacbe9d05ee7b90addb0d8ae9e62ee3",
+    "url": "/static/media/Macintosh.afacbe9d.mp4"
+  },
+  {
+    "revision": "c6e3ba55fce796b5aa421fecd3f9be9c",
+    "url": "/static/media/Morph.c6e3ba55.mp4"
+  },
+  {
+    "revision": "d82590476a06d92c0c7f52c37d6d1ad3",
+    "url": "/static/media/YouTubeintro-TechLila.d8259047.mp4"
+  },
+  {
+    "revision": "74011885e1019d57bce1b3133b3f53fd",
+    "url": "/static/media/A4 Copy1.74011885.jpg"
+  },
+  {
+    "revision": "079fb88a161055185764c9486ae745d0",
+    "url": "/static/media/Appk_2.079fb88a.jpg"
+  },
+  {
+    "revision": "6c92f40b3dd50dac3cbdd58163c5466d",
+    "url": "/static/media/PANTHEONMASTHEAD.6c92f40b.jpg"
+  },
+  {
+    "revision": "3ef2b498fe8c853886a95283d8edcbe2",
+    "url": "/static/media/poster1.3ef2b498.jpg"
+  },
+  {
+    "revision": "ca13b3329658fb8388161b0a86eeaeb1",
+    "url": "/static/media/shirt.ca13b332.JPG"
+  },
+  {
+    "revision": "023b3b3b8a8c056db210653e62b67d09",
+    "url": "/static/media/SPACEvents.023b3b3b.jpg"
+  },
+  {
+    "revision": "986cf9a9f77085a813327c4ac500a00b",
+    "url": "/static/media/reco-main.986cf9a9.png"
+  },
+  {
+    "revision": "3e1fdfeaa2e331281c97fd1613267c5e",
+    "url": "/static/media/homepage-flow-recotap.3e1fdfea.png"
+  },
+  {
+    "revision": "3681072e20259d4d6efbe70fcd37b64a",
+    "url": "/static/media/homepage-flow-reco.3681072e.png"
+  },
+  {
+    "revision": "3a3a8b1861e19744c225e8e696d8a154",
+    "url": "/index.html"
+  }
+];
